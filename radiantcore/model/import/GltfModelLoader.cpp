53921a562c5a1ccf5b3e51477e4e50b3fb3f9beb
blendogames <brendon@blendogames.com>
2026-08-06 17:16:44 -0700

    First pass at integrating pfirsich's gltf model loader

--- radiantcore/model/import/GltfModelLoader.cpp
+++ radiantcore/model/import/GltfModelLoader.cpp
@@ -11,6 +11,10 @@
 #include "parser/ParseException.h"
 #include "../picomodel/PicoModelLoader.h"
 
+//https://github.com/pfirsich/gltf
+#include "gltf.h"
+
+
 namespace model
 {
 
@@ -20,21 +24,48 @@ namespace model
 
     IModelPtr GltfModelLoader::loadModelFromPath(const std::string& path)
     {
-        // Open an ArchiveFile to load
-        auto file = path_is_absolute(path.c_str()) ?
-            GlobalFileSystem().openFileInAbsolutePath(path) :
-            GlobalFileSystem().openFile(path);
-
-        if (!file)
+        //// Open an ArchiveFile to load
+        //auto file = path_is_absolute(path.c_str()) ?
+        //    GlobalFileSystem().openFileInAbsolutePath(path) :
+        //    GlobalFileSystem().openFile(path);
+        
+        //if (!file)
+        //{
+        //    rError() << "Failed to load model " << path << std::endl;
+        //    return IModelPtr();
+        //}
+        //
+        //// Load the model data from the given stream
+        //archive::ScopedArchiveBuffer data(*file);
+        //
+        //return IModelPtr();
+        
+        const auto gltfFileOpt = gltf::load("D:\\games\\monstergame\\base\\models\\radio.glb");
+        if (!gltfFileOpt)
         {
-            rError() << "Failed to load model " << path << std::endl;
+            //Failed to load
+            return IModelPtr();
+        }
+        
+        const auto& gltfFile = *gltfFileOpt;
+        if (gltfFile.scenes.size() != 1)
+        {
+            //Too many scenes
             return IModelPtr();
         }
 
-        // Load the model data from the given stream
-        archive::ScopedArchiveBuffer data(*file);
+        for (size_t i = 0; i < gltfFile.materials.size(); ++i) {
+            const auto& gmaterial = gltfFile.materials[i];
 
-        
+            printf("Material name: %s", gmaterial.name);
+        }
+
+        //TODO
+
+
+
+
+        return IModelPtr();
     }
 
 }
